ebcb18d4c99781aa3b23f441fa80ed704d2078b7
Bkoji1150 <kojibello058@gmail.com>
2022-06-04 06:14:36 -0700

    topic: python modules

--- Modules/auth-program/main.py
+++ Modules/auth-program/main.py
@@ -1,20 +1,25 @@
 import getpass
-
-password = "kojitechs123"
 user_guess = ""
-user_atempt = 3 
 
 # first condition "if user_atempt not == 0", user_guess!=password
-
-while user_guess != password and user_atempt !=0:
+def main():
+    password = "kojitechs123"
+    user_atempt = 3 
     user_guess = getpass.getpass("Enter password: ")
-    user_atempt -=1
-    if user_atempt==0: 
-        print("Sorry you have exhausted the limit of 3")  
-        
-    if user_guess == password:
-        print("Login succesful!")
-        
-    else:
-        print(f"You have {user_atempt} attempt left")
-    
+    while user_guess != password and user_atempt !=0:
+        user_atempt -=1
+        if user_atempt==0: 
+            print("Sorry you have exhausted the limit of 3")  
+            
+        if user_guess == password:
+            print("Login succesful!")
+            
+        else:
+            print(f"You have {user_atempt} attempt left")
+    return None        
+
+try:
+    if __name__ == '__main__':
+        main()
+except Exception as e:
+    print(e)    
---
+++ Modules/platform-module/main.py
@@ -0,0 +1,5 @@
+
+import platform
+from pprint import pprint
+
+pprint(dir(platform))
